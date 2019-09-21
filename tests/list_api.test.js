0613aaeb62ccd7129a344f880755f6a78ae27315
Ilkka Hauta-aho <ilkka.hauta-aho@miiliconsulting.com>
2019-09-21 11:02:06 +0300

    4.9*: blogilistan testit, step2

--- tests/list_api.test.js
+++ tests/list_api.test.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Blog = require('../models/blog')
 const api = supertest(app)
+const logger = require('../utils/logger')
+
+const initialBlogs = [
+    {
+        title: 'Yle uriheilu',
+        author: 'Kari Mänty',
+        url: 'https://www.yle.fi/urheilu',
+        likes: 5,
+    },
+    {
+        title: 'Jatkoaika.com',
+        author: 'Jatkoaika toimitus',
+        url: 'https://www.jatkoaika.com',
+        likes: 53,
+    }
+]
+
+beforeEach(async () => {
+    logger.test('Deleting Blogs')
+    await Blog.deleteMany({})
+
+    let noteObject = new Blog(initialBlogs[0])
+    await noteObject.save()
+
+    noteObject = new Blog(initialBlogs[1])
+    await noteObject.save()
+
+    logger.test('Initial blogs stored to DB')
+})
 
 describe('Getting notes tests', () => {
 
@@ -10,6 +39,17 @@ describe('Getting notes tests', () => {
         await api.get('/api/blogs')
             .expect(200)
             .expect('Content-Type', 'application/json; charset=utf-8')
+    })
+
+    test('All blogs are returned', async () => {
+        const blogs = await api.get('/api/blogs')
+
+        expect(blogs.body.length).toBe(initialBlogs.length)
+    })
+
+    test('Blogs are identified by id', async () => {
+        const blogs = await api.get('/api/blogs')
+        blogs.body.forEach(b => expect(b).toBeDefined())
     })
 })
 
--- utils/logger.js
+++ utils/logger.js
@@ -4,10 +4,16 @@ const info = (...params) => {
     }
 }
 
+const test = (...params) => {
+    if (process.env.NODE_ENV === 'test') {
+        console.log(...params)
+    }
+}
+
 const error = (...params) => {
     console.error(...params)
 }
 
 module.exports = {
-    info, error
+    info, test, error
 }
